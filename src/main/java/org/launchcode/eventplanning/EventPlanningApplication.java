1520b71ca5f7e7968df75da1da866283d618278c
UshaLaunchcode <usha_ece@yahoo.com>
2021-04-13 18:34:50 -0500

    Finished Spring Boot project Initialization

---
+++ src/main/java/org/launchcode/eventplanning/EventPlanningApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.eventplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventPlanningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/eventplanning/EventPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.eventplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
