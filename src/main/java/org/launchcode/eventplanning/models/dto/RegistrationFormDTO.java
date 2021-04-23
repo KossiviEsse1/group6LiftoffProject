465b3196fdf0a569b2b5020d2ebe97841f44ac45
Fionna <fionnagemzon@gmail.com>
2021-04-22 20:26:35 -0500

    startTemplate

---
+++ src/main/java/org/launchcode/eventplanning/models/dto/RegistrationFormDTO.java
@@ -0,0 +1,13 @@
+package org.launchcode.eventplanning.models.DTO;
+
+public class RegistrationFormDTO extends LoginFormDTO {
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+}
