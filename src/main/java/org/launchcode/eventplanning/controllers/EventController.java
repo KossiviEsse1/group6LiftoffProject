54db57fddad446d74e785c58419107bce8297e1b
Pimpod <zmartin2998@gmail.com>
2021-04-27 20:23:59 -0500

    Edit Feature

--- src/main/java/org/launchcode/eventplanning/controllers/EventController.java
+++ src/main/java/org/launchcode/eventplanning/controllers/EventController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 
 @Controller
 public class EventController {
@@ -37,6 +39,34 @@ public class EventController {
         }
         eventRepository.save(newEvent);
         return "redirect:";
+    }
+
+    @GetMapping("update/{eventID}")
+    public String displayUpdateEventForm(Model model, @PathVariable int eventID){
+        Optional optEvent = eventRepository.findById(eventID);
+        if (optEvent.isPresent()) {
+            Event event = (Event) optEvent.get();
+        model.addAttribute("event", event);
+        return "update";
+        } else {
+            return "redirect:../";
+        }
+    }
+
+
+    @PostMapping("update/{eventID}")
+    public String processUpdateEventForm(@ModelAttribute Event currentEvent,
+                                      Errors errors, Model model, @PathVariable int eventID) {
+        if (errors.hasErrors()){
+            model.addAttribute("title","Update Event");
+            return "update";
+        }
+        Event currentEvent1 = eventRepository.findById(eventID).orElse(new Event());
+        currentEvent1.setDescription(currentEvent.getDescription());
+        currentEvent1.setLocation(currentEvent.getLocation());
+        currentEvent1.setName(currentEvent.getName());
+        eventRepository.save(currentEvent1);
+        return "redirect:/";
     }
 
 }
