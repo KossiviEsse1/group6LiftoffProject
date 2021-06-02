73708d6bf18e5009b78c77f7edcd4a03b20f11f0
Fionna <fionnagemzon@gmail.com>
2021-06-01 19:48:47 -0500

    cpulling

--- src/main/java/org/launchcode/eventplanning/controllers/EventController.java
+++ src/main/java/org/launchcode/eventplanning/controllers/EventController.java
@@ -4,13 +4,13 @@ import org.launchcode.eventplanning.models.DTO.EventVolunteerDTO;
 import org.launchcode.eventplanning.models.Event;
 import org.launchcode.eventplanning.models.User;
 import org.launchcode.eventplanning.models.data.EventRepository;
-import org.launchcode.eventplanning.models.data.EventUserRepository;
 import org.launchcode.eventplanning.models.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
@@ -25,44 +25,38 @@ public class EventController {
     private UserRepository userRepository;
     @Autowired
     private AuthenticationController authenticationController;
-    @Autowired
-    private EventUserRepository eventUserRepository;
-
 
     @RequestMapping("events")
-    public String index(Model model, HttpServletRequest request) {
+    public String index(Model model, HttpServletRequest request){
+        model.addAttribute("title", "Events");
         model.addAttribute("events", eventRepository.findAll());
         HttpSession session = request.getSession();
-        model.addAttribute("title", "Events");
         return "events";
     }
-
     @GetMapping("add")
     public String displayAddEventForm(Model model, HttpServletRequest request) {
         HttpSession session = request.getSession();
 
         User user = authenticationController.getUserFromSession(session);
-        if (user.getRole().equals("organization")) {
+        if(user.getRole().equals("organization")) {
             model.addAttribute("title", "Create Event");
             model.addAttribute(new Event());
             return "add";
         }
         return "redirect:";
     }
-
     @PostMapping("add")
     public String processAddEventForm(@ModelAttribute Event newEvent,
                                       Errors errors, Model model) {
-        if (errors.hasErrors()) {
-            model.addAttribute("title", "Create Event");
+        if (errors.hasErrors()){
+            model.addAttribute("title","Create Event");
             return "add";
         }
         eventRepository.save(newEvent);
         return "redirect:";
     }
-
     @GetMapping("update/{eventID}")
-    public String displayUpdateEventForm(Model model, @PathVariable int eventID) {
+    public String displayUpdateEventForm(Model model, @PathVariable int eventID){
         Optional optEvent = eventRepository.findById(eventID);
         if (optEvent.isPresent()) {
             Event event = (Event) optEvent.get();
@@ -73,12 +67,11 @@ public class EventController {
             return "redirect:../";
         }
     }
-
     @PostMapping("update/{eventID}")
     public String processUpdateEventForm(@ModelAttribute Event currentEvent,
                                          Errors errors, Model model, @PathVariable int eventID) {
-        if (errors.hasErrors()) {
-            model.addAttribute("title", "Update Event");
+        if (errors.hasErrors()){
+            model.addAttribute("title","Update Event");
             return "update";
         }
         Event currentEvent1 = eventRepository.findById(eventID).orElse(new Event());
@@ -88,23 +81,20 @@ public class EventController {
         eventRepository.save(currentEvent1);
         return "redirect:/";
     }
-
     @GetMapping("delete/{eventID}")
-    public String displayDeleteEventForm(Model model, @PathVariable int eventID) {
+    public String displayDeleteEventForm(Model model, @PathVariable int eventID){
         model.addAttribute("title", "Delete Events");
         Event event = eventRepository.findById(eventID).orElse(new Event());
         model.addAttribute("event", event);
         return "delete";
     }
-
     @PostMapping("delete/{eventID}")
-    public String processDeleteEventForm(@RequestParam String yesOrNo, @RequestParam int eventID) {
+    public String processDeleteEventForm(@RequestParam String yesOrNo, @RequestParam int eventID){
         if (yesOrNo.equals("yes")) {
             eventRepository.deleteById(eventID);
         }
         return "redirect:/";
     }
-
     @GetMapping("view/{eventID}")
     public String displayViewEventForm(Model model, @PathVariable int eventID) {
         Optional optEvent = eventRepository.findById(eventID);
@@ -112,22 +102,14 @@ public class EventController {
             Event event = (Event) optEvent.get();
             model.addAttribute("title", "View Event Details");
             model.addAttribute("event", event);
+/*
+            model.addAttribute("volunteers", "event.getUsers");
+*/
             return "view";
-        } else {
+        }else{
             model.addAttribute("events", eventRepository.findAll());
             return "redirect:../";
         }
-    }
-
-    @PostMapping("view/{eventID}")
-    public String processViewEventForm(@ModelAttribute Event currentEvent,
-                                       Errors errors, Model model, @PathVariable int eventID) {
-        if (errors.hasErrors()) {
-            model.addAttribute("title", "View Event");
-            return "view";
-        }
-        Optional<Event> currentEvent2 = eventRepository.findById(eventID);
-        return "redirect:/";
     }
 
     @GetMapping("signup")
@@ -149,12 +131,12 @@ public class EventController {
     @PostMapping("signup")
     public String processAddTagForm(@ModelAttribute @Valid EventVolunteerDTO eventVolunteer,
                                     Errors errors,
-                                    Model model) {
+                                    Model model){
 
         if (!errors.hasErrors()) {
             Event event = eventVolunteer.getEvent();
             User user = eventVolunteer.getUser();
-            if (!event.getVolunteers().contains(user)) {
+            if (!event.getVolunteers().contains(user)){
                 System.out.println(user);
                 event.addVolunteer(user);
                 eventRepository.save(event);
@@ -163,6 +145,8 @@ public class EventController {
             }
             return "events";
         }
-    }
-}
 
+        return "events";
+    }
+
+}
